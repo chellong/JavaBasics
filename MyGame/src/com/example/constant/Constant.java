b563c3fe24750a41646ee4a0a5a8fd65e6573cc6
chellong <352977335@qq.com>
2015-12-21 17:31:23 +0800

    SolarFrame start

---
+++ MyGame/src/com/example/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.example.constant;
+
+public class Constant {
+	
+	public static int GAME_WIDTH = 500;
+	public static int GAME_HEIGHT = 500;
+}
--- MyGame/src/com/example/game/GameFrame.java
+++ MyGame/src/com/example/game/GameFrame.java
@@ -7,8 +7,24 @@ import java.awt.Image;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import com.example.util.GameUtil;
+
 /**
  * GUI變成，awt，swing
+ * 
+ * //无填充
+ *		g.drawLine(100, 100, 200, 200);
+ *		g.drawRect(100, 100, 200, 200);//矩形
+ *		g.drawOval(100, 100, 200, 200);//圆
+ *		
+ *		//Font font = 
+ *		g.drawString("123", 200,200);
+ *		//填充，图
+ *		g.fillRect(100, 100, 20, 20);
+ *		Color c = g.getColor();
+ *		g.setColor(new Color(255, 0, 0));
+ *		g.fillOval(300, 300, 5,5);
+ *		g.setColor(c);
  * 
  * @author 北飞的候鸟
  *
@@ -20,7 +36,8 @@ public class GameFrame extends Frame{
 	 */
 	private static final long serialVersionUID = 3187915630805806272L;
 
-	//Image img = GameUtil.getImage("images/1449072742.jpg");
+	private Image img = GameUtil.getImage("com/example/images/1449072742.jpg");
+	
 	public void launchFrame(){
 		this.setBackground(Color.white);
 		this.setSize(500, 500);
@@ -36,27 +53,38 @@ public class GameFrame extends Frame{
 		});
 	}
 	
+	private int x = 0,y = 0;
+	
 	@Override
 	public void paint(Graphics g) {
 		
-		//无填充
-		g.drawLine(100, 100, 200, 200);
-		g.drawRect(100, 100, 200, 200);//矩形
-		g.drawOval(100, 100, 200, 200);//圆
+		g.drawImage(img,x,y,null);
 		
-		//Font font = 
-		g.drawString("123", 200,200);
-		//填充，图
-		g.fillRect(100, 100, 20, 20);
-		Color c = g.getColor();
-		g.setColor(new Color(255, 0, 0));
-		g.fillOval(300, 300, 5,5);
-		g.setColor(c);
+		new PaintThread().start();
 		
-		
-		//g.drawImage(img,200,200,null);
 	}
 	
+	/**
+	 * 
+	 * 重新绘制界面
+	 * 
+	 * @author 北飞的候鸟
+	 *
+	 */
+	class PaintThread extends Thread{
+		
+		@Override
+		public void run() {
+			while (true) {
+				repaint();
+				try {
+					Thread.sleep(40);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 	public static void main(String[] args) {
 		
 		GameFrame frame = new GameFrame();

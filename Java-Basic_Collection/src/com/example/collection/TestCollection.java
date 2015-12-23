5e5aa77130016715171030e0f1ac654d2e0c336c
chellong <352977335@qq.com>
2015-12-23 10:20:22 +0800

    15，12，13
    Collection

---
+++ Java-Basic_Collection/src/com/example/collection/TestCollection.java
@@ -0,0 +1,20 @@
+package com.example.collection;
+
+import java.util.Collection;
+
+/**
+ * arrayList 底层是数组，查询快，修改，插入，删除慢
+ * 
+ * LinkedList 底层链表，查询慢，修改，插入，删除快
+ * 
+ * Vector 线程安全，效率低
+ * 
+ * @author 北飞的候鸟
+ *
+ */
+public class TestCollection {
+
+	public static void main(String[] args) {
+		
+	}
+}

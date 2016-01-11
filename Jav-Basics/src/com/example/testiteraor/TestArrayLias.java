d4e4fcf0f60a7a9eada70332d06eb02afe47689f
chellong <352977335@qq.com>
2016-01-11 22:43:00 +0800

    16，01，11 ，01 regular expression
    regular expression

---
+++ Jav-Basics/src/com/example/testiteraor/TestArrayLias.java
@@ -0,0 +1,11 @@
+package com.example.testiteraor;
+
+import java.util.ArrayList;
+
+public class TestArrayLias {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		System.out.println(list.size());
+	}
+}

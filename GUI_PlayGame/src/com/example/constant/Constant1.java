a955aac182ffe51faba25fd6e4e9fe28e8afb703
chellong <352977335@qq.com>
2016-01-12 23:00:45 +0800

    16，01，12，00

---
+++ GUI_PlayGame/src/com/example/constant/Constant1.java
@@ -0,0 +1,7 @@
+package com.example.constant;
+
+public class Constant1 {
+
+	public static int GAME_WIDTH = 400;
+	public static int GAME_HEIGHT = 600;
+}
--- SolarSystem/src/com/example/constant/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.example.constant;
-
-public class Constant {
-	
-	public static int GAME_WIDTH = 800;
-	public static int GAME_HEIGHT = 800;
-}
